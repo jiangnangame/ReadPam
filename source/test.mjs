dd3a4a001c0b6b3540d6c5cb301195321737cd16
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2020-04-13 02:28:14 +0800

    Delete test.mjs

--- source/test.mjs
+++
@@ -1,6 +0,0 @@
-"use strict";
-import ReadPam from './ReadPam.mjs';
-import fs from 'fs';
-const PamName = process.argv[2];
-!fs.existsSync('./output/') && fs.mkdirSync('./output/');
-new ReadPam(PamName).start(`./output/`);
